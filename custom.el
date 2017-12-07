78357f979c05a4f793def499b6f0688a358a48db
Zhao Pei <zhaopei8948@hotmail.com>
2017-12-07 11:00:14 +0800

    添加evil及evil-leader,及evil-surround插件

--- custom.el
+++ custom.el
@@ -9,7 +9,7 @@
     ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(package-selected-packages
    (quote
-    (tabbar-ruler highlight-symbol popup-complete auto-yasnippet yasnippet auto-complete-c-headers auto-complete-clang auto-complete uptimes crontab-mode yagist whole-line-or-region whitespace-cleanup-mode wgrep-ag vc-darcs unfill undo-tree symbol-overlay switch-window smex session scratch rainbow-delimiters projectile project-local-variables page-break-lines nlinum multiple-cursors move-dup mmm-mode magit-gh-pulls list-unicode-display ivy-historian indent-guide ibuffer-vc highlight-escape-sequences guide-key gitignore-mode github-issues github-clone gitconfig-mode git-timemachine git-messenger git-blamed fullframe flycheck-color-mode-line fill-column-indicator expand-region exec-path-from-shell disable-mouse dired-sort dired+ diminish diff-hl default-text-scale darcsum counsel company-quickhelp command-log-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized cmd-to-echo bug-reference-github browse-kill-ring browse-at-remote avy anzu alert ag)))
+    (use-package evil-surround evil-leader evil tabbar-ruler highlight-symbol popup-complete auto-yasnippet yasnippet auto-complete-c-headers auto-complete-clang auto-complete uptimes crontab-mode yagist whole-line-or-region whitespace-cleanup-mode wgrep-ag vc-darcs unfill undo-tree symbol-overlay switch-window smex session scratch rainbow-delimiters projectile project-local-variables page-break-lines nlinum multiple-cursors move-dup mmm-mode magit-gh-pulls list-unicode-display ivy-historian indent-guide ibuffer-vc highlight-escape-sequences guide-key gitignore-mode github-issues github-clone gitconfig-mode git-timemachine git-messenger git-blamed fullframe flycheck-color-mode-line fill-column-indicator expand-region exec-path-from-shell disable-mouse dired-sort dired+ diminish diff-hl default-text-scale darcsum counsel company-quickhelp command-log-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized cmd-to-echo bug-reference-github browse-kill-ring browse-at-remote avy anzu alert ag)))
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -20,22 +20,24 @@
 (global-linum-mode t)
 (column-number-mode t)
 
-(require 'auto-complete-config)(ac-config-default)
-(require 'yasnippet)(yas/global-mode 1)
-(require 'auto-complete-clang)
+(maybe-require-package 'auto-complete)
+(maybe-require-package 'auto-complete-mode)
+(maybe-require-package 'auto-complete-config)(ac-config-default)
+(maybe-require-package 'yasnippet)(yas/global-mode 1)
+(maybe-require-package 'auto-complete-clang)
 (setq ac-auto-start t)
 (setq ac-quick-help-delay 0.5)
 (define-key ac-mode-map [(control tab)] 'auto-complete)
 ;; 使用这命令echo "" | g++ -v -x c++ -E - 查看头文件导入的路径
 (defun my-ac-config () (setq ac-clang-flags (mapcar(lambda (item)(concat "-I" item))
                                                    (split-string "
-/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
+/usr/lib/gcc/x86_64-pc-linux-gnu/7.2.0/../../../../include/c++/7.2.0
+/usr/lib/gcc/x86_64-pc-linux-gnu/7.2.0/../../../../include/c++/7.2.0/x86_64-pc-linux-gnu
+/usr/lib/gcc/x86_64-pc-linux-gnu/7.2.0/../../../../include/c++/7.2.0/backward
+/usr/lib/gcc/x86_64-pc-linux-gnu/7.2.0/include
 /usr/local/include
-/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/8.0.0/include
-/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include
+/usr/lib/gcc/x86_64-pc-linux-gnu/7.2.0/include-fixed
 /usr/include
-/System/Library/Frameworks
-/Library/Frameworks
 ")))
        (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
        (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
@@ -50,7 +52,7 @@
 ;; ac-source-gtags
 (my-ac-config)
 ;; setting highlight-symbol
-(require 'highlight-symbol)
+(maybe-require-package 'highlight-symbol)
 (global-set-key [(control f3)] 'highlight-symbol)
 (global-set-key [f3] 'highlight-symbol-next)
 (global-set-key [(shift f3)] 'highlight-symbol-prev)
@@ -61,5 +63,20 @@
 (setq tabbar-ruler-global-ruler t)     ; get global ruler
 (setq tabbar-ruler-popup-menu t)       ; get popup menu.
 (setq tabbar-ruler-popup-toolbar t)    ; get popup toolbar
-(setq tabbar-ruler-popup-scrollbar t)  ; show scroll-bar on mouse-move
-(require 'tabbar-ruler)
+(setq tabbar-ruler-popup-scrollbar t)  ; show scroll-bar on mouse
+(maybe-require-package 'tabbar-ruler)
+
+;; evil setting
+(maybe-require-package 'evil)
+(evil-mode 1)
+
+;; evil-leader setting
+(maybe-require-package 'evil-leader)
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+
+;; evil-surround setting
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
