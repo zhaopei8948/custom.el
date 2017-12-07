ac59fa23181ee093e40f1a4765ed39dce17245d2
Zhao Pei <zhaopei8948@hotmail.com>
2017-12-07 11:57:49 +0800

    添加evil-nerd-commenter

--- custom.el
+++ custom.el
@@ -9,7 +9,7 @@
     ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(package-selected-packages
    (quote
-    (use-package evil-surround evil-leader evil tabbar-ruler highlight-symbol popup-complete auto-yasnippet yasnippet auto-complete-c-headers auto-complete-clang auto-complete uptimes crontab-mode yagist whole-line-or-region whitespace-cleanup-mode wgrep-ag vc-darcs unfill undo-tree symbol-overlay switch-window smex session scratch rainbow-delimiters projectile project-local-variables page-break-lines nlinum multiple-cursors move-dup mmm-mode magit-gh-pulls list-unicode-display ivy-historian indent-guide ibuffer-vc highlight-escape-sequences guide-key gitignore-mode github-issues github-clone gitconfig-mode git-timemachine git-messenger git-blamed fullframe flycheck-color-mode-line fill-column-indicator expand-region exec-path-from-shell disable-mouse dired-sort dired+ diminish diff-hl default-text-scale darcsum counsel company-quickhelp command-log-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized cmd-to-echo bug-reference-github browse-kill-ring browse-at-remote avy anzu alert ag)))
+    (evil-matchit evil-nerd-commenter auto-complete-config auto-complete-modue use-package evil-surround evil-leader evil tabbar-ruler highlight-symbol popup-complete auto-yasnippet yasnippet auto-complete-c-headers auto-complete-clang auto-complete uptimes crontab-mode yagist whole-line-or-region whitespace-cleanup-mode wgrep-ag vc-darcs unfill undo-tree symbol-overlay switch-window smex session scratch rainbow-delimiters projectile project-local-variables page-break-lines nlinum multiple-cursors move-dup mmm-mode magit-gh-pulls list-unicode-display ivy-historian indent-guide ibuffer-vc highlight-escape-sequences guide-key gitignore-mode github-issues github-clone gitconfig-mode git-timemachine git-messenger git-blamed fullframe flycheck-color-mode-line fill-column-indicator expand-region exec-path-from-shell disable-mouse dired-sort dired+ diminish diff-hl default-text-scale darcsum counsel company-quickhelp command-log-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized cmd-to-echo bug-reference-github browse-kill-ring browse-at-remote avy anzu alert ag)))
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -21,10 +21,12 @@
 (column-number-mode t)
 
 (maybe-require-package 'auto-complete)
-(maybe-require-package 'auto-complete-mode)
+(maybe-require-package 'auto-complete-modue)
 (maybe-require-package 'auto-complete-config)(ac-config-default)
-(maybe-require-package 'yasnippet)(yas/global-mode 1)
+(maybe-require-package 'yasnippet)
+(yas-global-mode 1)
 (maybe-require-package 'auto-complete-clang)
+(maybe-require-package 'auto-complete-c-headers)
 (setq ac-auto-start t)
 (setq ac-quick-help-delay 0.5)
 (define-key ac-mode-map [(control tab)] 'auto-complete)
@@ -47,8 +49,9 @@
        (add-hook 'auto-complete-mode-hook 'ac-common-setup)
        (global-auto-complete-mode t))
 (defun my-ac-cc-mode-setup ()
-  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
+  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet ac-source-c-headers) ac-sources)))
 (add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
+(add-hook 'c++-mode-hook 'my-ac-cc-mode-setup)
 ;; ac-source-gtags
 (my-ac-config)
 ;; setting highlight-symbol
@@ -59,12 +62,12 @@
 (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
 
 ;; setting tabbar-ruler
+(maybe-require-package 'tabbar-ruler)
 (setq tabbar-ruler-global-tabbar t)    ; get tabbar
 (setq tabbar-ruler-global-ruler t)     ; get global ruler
 (setq tabbar-ruler-popup-menu t)       ; get popup menu.
 (setq tabbar-ruler-popup-toolbar t)    ; get popup toolbar
 (setq tabbar-ruler-popup-scrollbar t)  ; show scroll-bar on mouse
-(maybe-require-package 'tabbar-ruler)
 
 ;; evil setting
 (maybe-require-package 'evil)
@@ -75,8 +78,12 @@
 (global-evil-leader-mode)
 (evil-leader/set-leader ",")
 
-;; evil-surround setting
+;;       evil-surround setting
 (use-package evil-surround
   :ensure t
   :config
   (global-evil-surround-mode 1))
+
+;; evil-nerd-commenter
+(maybe-require-package 'evil-nerd-commenter)
+(evilnc-default-hotkeys)
